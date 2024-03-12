be055d2dbc5efd6e1f5f326d31d5fd035a5df6bc
laxmi-mehta <mehtalaxmi075@gmail.com>
2024-03-12 16:35:52 +0530

    Initial Commit

--- style.js
+++ style.js
@@ -1,7 +1,24 @@
-var debounce = function(fn, t = 1000) {
-    let timer;
-    return function(...args) {
-        clearTimeout(timer);
-        timer = setTimeout(() => fn(...args), t);
+// var debounce = function(fn, t = 1000) {
+//     let timer;
+//     return function(...args) {
+//         clearTimeout(timer);
+//         timer = setTimeout(() => fn(...args), t);
+//     }
+// };
+// Get Variables
+const toggleMenuClicked = () => {
+    const body = document.body;
+    const openIcon = document.getElementById("open-icon");
+    const closeIcon = document.getElementById("close-icon");
+  
+    body.classList.toggle("open");
+  
+    if (body.classList.contains("open")) {
+      openIcon.style.display = "none";
+      closeIcon.style.display = "flex";
+    } else {
+      openIcon.style.display = "flex";
+      closeIcon.style.display = "none";
     }
-};
+  };
+  
